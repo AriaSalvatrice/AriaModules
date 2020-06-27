37c5a1c5b318a0a32d543a93789804a43540bc07
Aria Salvatrice <woof@aria.dog>
2020-06-27 18:07:05 +0200

    The basics of the module work now

--- src/Qqqq.cpp
+++ src/Qqqq.cpp
@@ -67,7 +67,6 @@ struct Qqqq : Module {
     std::array<bool, 12> litKeys;
     Lcd::LcdStatus lcdStatus;
     dsp::ClockDivider refreshScaleDivider;
-    dsp::ClockDivider litKeysDivider;
     
     Qqqq() {
         config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
@@ -97,7 +96,6 @@ struct Qqqq : Module {
         configParam(SCENE_BUTTON_PARAM, 0.f, 1.f, 0.f, "Scene #1");
         for (int i = 1; i < 16; i++) configParam(SCENE_BUTTON_PARAM + i, 0.f, 1.f, 0.f, "Scene #" + std::to_string(i + 1));
         refreshScaleDivider.setDivision(DISPLAYDIVIDER);
-        litKeysDivider.setDivision(8000);
         lcdMode = INIT_MODE;
         lcdLastInteraction = 0.f;
         lcdStatus.lcdText1 = " Q< quack~";
@@ -200,9 +198,13 @@ struct Qqqq : Module {
         }
     }
 
+    void cleanLitKeys() {
+        for (int i =  0; i < 12; i++) litKeys[i] = false; 
+    }
+
     void updateLitKeys() {
         // Test
-        for (int i =  0; i < 12; i++) litKeys[i] = (random::uniform() > 0.8) ? true : false; 
+        // for (int i =  0; i < 12; i++) litKeys[i] = (random::uniform() > 0.8) ? true : false; 
     }
 
 
@@ -212,12 +214,44 @@ struct Qqqq : Module {
         
         // Stop if no input
         if (channels == 0) return;
+        // Stop if no output while visualization is not enabled
+        if (! outputs[CV_OUTPUT + col].isConnected() && params[VISUALIZE_PARAM + col].getValue() == 0.f ) return;
 
-        // Get input
-        for (int i = 0; i < channels; i++) voltage[i] = inputs[CV_INPUT].getVoltage(i);
+        for (int i = 0; i < channels; i++) {
+            // Read input
+            // FIXME: S&H
+            voltage[i] = inputs[CV_INPUT + col].getVoltage(i);
+            // Scale and offset
+            voltage[i] = voltage[i] * params[SCALING_PARAM + col].getValue() / 100.f;
+            voltage[i] = voltage[i] + params[OFFSET_PARAM + col].getValue();
+            // Quantize in transpose mode 0: Octaves
+            if (params[TRANSPOSE_MODE_PARAM + col].getValue() == 0.f) {
+                voltage[i] = Quantizer::quantize(voltage[i], scale[scene]);
+                voltage[i] = voltage[i] + params[TRANSPOSE_PARAM + col].getValue();
+                voltage[i] = clamp(voltage[i], -10.f, 10.f);
+            }
+            // Quantize in transpose mode 1: Semitones
+            if (params[TRANSPOSE_MODE_PARAM + col].getValue() == 1.f) {
+                voltage[i] = voltage[i] + params[TRANSPOSE_PARAM + col].getValue() * 1.f / 12.f;
+                voltage[i] = Quantizer::quantize(voltage[i], scale[scene]);
+            }
+            // Quantize in transpose mode 2: Scale degrees
+            // FIXME: This one gonna be hard!!!!!
+            if (params[TRANSPOSE_MODE_PARAM + col].getValue() == 2.f) {
+                voltage[i] = Quantizer::quantize(voltage[i], scale[scene]);
+            }
 
-        // Output
-        for (int i = 0; i < channels; i++) outputs[CV_OUTPUT + col].setVoltage(voltage[i]);
+            // Piano display
+            if (params[VISUALIZE_PARAM + col].getValue() == 1.f) {
+                float v = voltage[i] * 12.f + 60.f; // Must be positive to work
+                int n = (int) v % 12;
+                litKeys[n] = true;
+            }
+
+            // Output
+            outputs[CV_OUTPUT + col].setVoltage(voltage[i], i);
+        }
+
         outputs[CV_OUTPUT + col].setChannels(channels);
     }
 
@@ -226,46 +260,55 @@ struct Qqqq : Module {
             updateScene();
             updateScale();
         }
+        cleanLitKeys();
         for(int i = 0; i < 4; i++) processQuantizerColumn(i);
         updateExternalOutput();
-        if (litKeysDivider.process()) {
-            updateLitKeys();
-        }
     }
 };
 
 
 
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+namespace QqqqWidgets {
 
 // The LCD knobs
-// FIXME: LOTS OF NAMESPACE ISSUES. I should namespace everything.
-// FIXME: dynamic_cast isn't working here. I dunno why!!
-// FIXME: THEY CRASH! Possible for a different reason than I expect.
-
-struct AriaKnob820ScaleQqqq : AriaKnob820 {
-    AriaKnob820ScaleQqqq() {
-        snap = true;
-        AriaKnob820();
+struct LcdKnob : AriaKnob820 {
+    void onDragMove(const event::DragMove& e) override {
+         dynamic_cast<Qqqq*>(paramQuantity->module)->lcdLastInteraction = 0.f;
+         dynamic_cast<Qqqq*>(paramQuantity->module)->lcdStatus.lcdDirty = true;
+        AriaKnob820::onDragMove(e);
     }
-
+};
+struct ScaleKnob : LcdKnob {
+    ScaleKnob() {
+        snap = true;
+        LcdKnob();
+    }
     void onDragMove(const event::DragMove& e) override {
         dynamic_cast<Qqqq*>(paramQuantity->module)->lcdMode = SCALE_MODE;
-        dynamic_cast<Qqqq*>(paramQuantity->module)->lcdLastInteraction = 0.f;
-        dynamic_cast<Qqqq*>(paramQuantity->module)->lcdStatus.lcdDirty = true;
-        AriaKnob820::onDragMove(e);
+        LcdKnob::onDragMove(e);
+    }
+};
+struct TransposeKnob : LcdKnob {
+    TransposeKnob() {
+        snap = true;
+        LcdKnob();
+    }
+    void onDragMove(const event::DragMove& e) override {
+        dynamic_cast<Qqqq*>(paramQuantity->module)->lcdMode = TRANSPOSE_MODE;
+        LcdKnob::onDragMove(e);
     }
 };
 
 
 // The piano display
 // https://community.vcvrack.com/t/whats-the-best-way-to-implement-a-pushbutton-with-three-visual-states-but-only-two-user-controllable-states/10351/8?u=aria_salvatrice
-struct AriaPianoKey : SvgSwitchUnshadowed {
+struct PianoKey : SvgSwitchUnshadowed {
     bool lastPianoDisplay = false;
     bool currentPianoDisplay = false;
     int note = 0;
@@ -290,78 +333,78 @@ struct AriaPianoKey : SvgSwitchUnshadowed {
 };
 
 // To represent an octave, only 6 distinct key graphics are required: C, E, F, B, White with notch on each side, Black
-struct AriaPianoWhiteKey : AriaPianoKey {
-    AriaPianoWhiteKey() {
+struct PianoWhiteKey : PianoKey {
+    PianoWhiteKey() {
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-white.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-white.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-white.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
-struct AriaPianoBlackKey : AriaPianoKey {
-    AriaPianoBlackKey() {
+struct PianoBlackKey : PianoKey {
+    PianoBlackKey() {
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-black.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-black.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-black.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
-struct AriaPianoC : AriaPianoKey {
-    AriaPianoC() {
+struct PianoC : PianoKey {
+    PianoC() {
         note = 0;
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-C.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-C.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-C.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
-struct AriaPianoCSharp : AriaPianoBlackKey { AriaPianoCSharp() { note = 1; AriaPianoBlackKey();} };
-struct AriaPianoD : AriaPianoWhiteKey { AriaPianoD () { note = 2; AriaPianoWhiteKey(); } };
-struct AriaPianoDSharp : AriaPianoBlackKey { AriaPianoDSharp() { note = 3; AriaPianoBlackKey();} };
-struct AriaPianoE : AriaPianoKey {
-    AriaPianoE() {
+struct PianoCSharp : PianoBlackKey { PianoCSharp() { note = 1; PianoBlackKey();} };
+struct PianoD : PianoWhiteKey { PianoD () { note = 2; PianoWhiteKey(); } };
+struct PianoDSharp : PianoBlackKey { PianoDSharp() { note = 3; PianoBlackKey();} };
+struct PianoE : PianoKey {
+    PianoE() {
         note = 4;
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-E.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-E.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-E.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
-struct AriaPianoF : AriaPianoKey {
-    AriaPianoF() {
+struct PianoF : PianoKey {
+    PianoF() {
         note = 5;
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-F.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-F.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-F.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
-struct AriaPianoFSharp : AriaPianoBlackKey { AriaPianoFSharp() { note = 6; AriaPianoBlackKey();} };
-struct AriaPianoG : AriaPianoWhiteKey { AriaPianoG () { note = 7; AriaPianoWhiteKey(); } };
-struct AriaPianoGSharp : AriaPianoBlackKey { AriaPianoGSharp() { note = 8; AriaPianoBlackKey();} };
-struct AriaPianoA : AriaPianoWhiteKey { AriaPianoA () { note = 9; AriaPianoWhiteKey(); } };
-struct AriaPianoASharp : AriaPianoBlackKey { AriaPianoASharp() { note = 10; AriaPianoBlackKey();} };
-struct AriaPianoB : AriaPianoKey {
-    AriaPianoB() {
+struct PianoFSharp : PianoBlackKey { PianoFSharp() { note = 6; PianoBlackKey();} };
+struct PianoG : PianoWhiteKey { PianoG () { note = 7; PianoWhiteKey(); } };
+struct PianoGSharp : PianoBlackKey { PianoGSharp() { note = 8; PianoBlackKey();} };
+struct PianoA : PianoWhiteKey { PianoA () { note = 9; PianoWhiteKey(); } };
+struct PianoASharp : PianoBlackKey { PianoASharp() { note = 10; PianoBlackKey();} };
+struct PianoB : PianoKey {
+    PianoB() {
         note = 11;
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/unlit-B.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/yellow-B.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/piano-buttons/pink-B.svg")));
-        AriaPianoKey();
+        PianoKey();
     }
 };
 
 // Keyboard, clipboard
-struct AriaPushButtonKeyboard : SvgSwitchUnshadowed {
-    AriaPushButtonKeyboard() {
+struct PushButtonKeyboard : SvgSwitchUnshadowed {
+    PushButtonKeyboard() {
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/button-keyboard.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/button-keyboard-pressed.svg")));
         momentary = true;
         SvgSwitchUnshadowed();
     }
 };
-struct AriaPushButtonClipboard : SvgSwitchUnshadowed {
-    AriaPushButtonClipboard() {
+struct PushButtonClipboard : SvgSwitchUnshadowed {
+    PushButtonClipboard() {
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/button-clipboard.svg")));
         addFrame(APP->window->loadSvg(asset::plugin(pluginInstance, "res/components/button-clipboard-pressed.svg")));
         momentary = true;
@@ -370,11 +413,21 @@ struct AriaPushButtonClipboard : SvgSwitchUnshadowed {
 };
 
 // Scene buttons, we'll give them frames later.
-struct AriaSceneButton : SvgSwitchUnshadowed {
-    AriaSceneButton() {
+struct SceneButton : SvgSwitchUnshadowed {
+    SceneButton() {
         SvgSwitch();
     }
 };
+
+} // namespace QqqqWidgets
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
 
@@ -389,26 +442,26 @@ struct QqqqWidget : ModuleWidget {
 
     void drawPianoKeys(float xOffset, float yOffset, Qqqq* module) {
         // First we create the white keys only.
-        addParam(createParam<AriaPianoC>(mm2px(Vec(xOffset, yOffset -  0.f)), module, Qqqq::NOTE_PARAM +  0)); // C
-        addParam(createParam<AriaPianoD>(mm2px(Vec(xOffset, yOffset - 14.f)), module, Qqqq::NOTE_PARAM +  2)); // D
-        addParam(createParam<AriaPianoE>(mm2px(Vec(xOffset, yOffset - 28.f)), module, Qqqq::NOTE_PARAM +  4)); // E
-        addParam(createParam<AriaPianoF>(mm2px(Vec(xOffset, yOffset - 42.f)), module, Qqqq::NOTE_PARAM +  5)); // F
-        addParam(createParam<AriaPianoG>(mm2px(Vec(xOffset, yOffset - 56.f)), module, Qqqq::NOTE_PARAM +  7)); // G
-        addParam(createParam<AriaPianoA>(mm2px(Vec(xOffset, yOffset - 70.f)), module, Qqqq::NOTE_PARAM +  9)); // A
-        addParam(createParam<AriaPianoB>(mm2px(Vec(xOffset, yOffset - 84.f)), module, Qqqq::NOTE_PARAM + 11)); // B
+        addParam(createParam<QqqqWidgets::PianoC>(mm2px(Vec(xOffset, yOffset -  0.f)), module, Qqqq::NOTE_PARAM +  0)); // C
+        addParam(createParam<QqqqWidgets::PianoD>(mm2px(Vec(xOffset, yOffset - 14.f)), module, Qqqq::NOTE_PARAM +  2)); // D
+        addParam(createParam<QqqqWidgets::PianoE>(mm2px(Vec(xOffset, yOffset - 28.f)), module, Qqqq::NOTE_PARAM +  4)); // E
+        addParam(createParam<QqqqWidgets::PianoF>(mm2px(Vec(xOffset, yOffset - 42.f)), module, Qqqq::NOTE_PARAM +  5)); // F
+        addParam(createParam<QqqqWidgets::PianoG>(mm2px(Vec(xOffset, yOffset - 56.f)), module, Qqqq::NOTE_PARAM +  7)); // G
+        addParam(createParam<QqqqWidgets::PianoA>(mm2px(Vec(xOffset, yOffset - 70.f)), module, Qqqq::NOTE_PARAM +  9)); // A
+        addParam(createParam<QqqqWidgets::PianoB>(mm2px(Vec(xOffset, yOffset - 84.f)), module, Qqqq::NOTE_PARAM + 11)); // B
         // Then, the black keys, so they overlap the clickable area of the white keys, avoiding the need he need for a custom widget.
-        addParam(createParam<AriaPianoCSharp>(mm2px(Vec(xOffset, yOffset -  5.f)), module, Qqqq::NOTE_PARAM +  1)); // C#
-        addParam(createParam<AriaPianoDSharp>(mm2px(Vec(xOffset, yOffset - 19.f)), module, Qqqq::NOTE_PARAM +  3)); // D#
-        addParam(createParam<AriaPianoFSharp>(mm2px(Vec(xOffset, yOffset - 47.f)), module, Qqqq::NOTE_PARAM +  6)); // F#
-        addParam(createParam<AriaPianoGSharp>(mm2px(Vec(xOffset, yOffset - 61.f)), module, Qqqq::NOTE_PARAM +  8)); // G#
-        addParam(createParam<AriaPianoASharp>(mm2px(Vec(xOffset, yOffset - 75.f)), module, Qqqq::NOTE_PARAM + 10)); // A#
+        addParam(createParam<QqqqWidgets::PianoCSharp>(mm2px(Vec(xOffset, yOffset -  5.f)), module, Qqqq::NOTE_PARAM +  1)); // C#
+        addParam(createParam<QqqqWidgets::PianoDSharp>(mm2px(Vec(xOffset, yOffset - 19.f)), module, Qqqq::NOTE_PARAM +  3)); // D#
+        addParam(createParam<QqqqWidgets::PianoFSharp>(mm2px(Vec(xOffset, yOffset - 47.f)), module, Qqqq::NOTE_PARAM +  6)); // F#
+        addParam(createParam<QqqqWidgets::PianoGSharp>(mm2px(Vec(xOffset, yOffset - 61.f)), module, Qqqq::NOTE_PARAM +  8)); // G#
+        addParam(createParam<QqqqWidgets::PianoASharp>(mm2px(Vec(xOffset, yOffset - 75.f)), module, Qqqq::NOTE_PARAM + 10)); // A#
     }
 
     void drawQuantizerColumn(float xOffset, float yOffset, Qqqq* module, int col) {
         addInput(createInput<AriaJackIn>(mm2px(Vec(xOffset + 0.f, yOffset + 0.f)), module, Qqqq::CV_INPUT + col));
         addParam(createParam<AriaKnob820>(mm2px(Vec(xOffset + 0.f, yOffset + 10.f)), module, Qqqq::SCALING_PARAM + col));
         addParam(createParam<AriaKnob820>(mm2px(Vec(xOffset + 0.f, yOffset + 20.f)), module, Qqqq::OFFSET_PARAM + col));
-        addParam(createParam<AriaKnob820>(mm2px(Vec(xOffset + 0.f, yOffset + 30.f)), module, Qqqq::TRANSPOSE_PARAM + col));
+        addParam(createParam<QqqqWidgets::TransposeKnob>(mm2px(Vec(xOffset + 0.f, yOffset + 30.f)), module, Qqqq::TRANSPOSE_PARAM + col));
 
         addParam(createParam<AriaPushButton500>(mm2px(Vec(xOffset + 3.5f, yOffset + 40.f)), module, Qqqq::TRANSPOSE_MODE_PARAM + col));
         addParam(createParam<AriaPushButton500>(mm2px(Vec(xOffset + -0.5f, yOffset + 42.5f)), module, Qqqq::SHTH_MODE_PARAM + col));
@@ -428,7 +481,7 @@ struct QqqqWidget : ModuleWidget {
                 } else {
                     scene = std::to_string(i + 1);
                 }
-                AriaSceneButton* o = new AriaSceneButton;
+                QqqqWidgets::SceneButton* o = new QqqqWidgets::SceneButton;
                 o->box.pos = mm2px(Vec(xOffset + x * 8.f, yOffset - y * 8.f));
                 if (module) {
                     o->paramQuantity = module->paramQuantities[Qqqq::SCENE_BUTTON_PARAM + i];
@@ -455,8 +508,8 @@ struct QqqqWidget : ModuleWidget {
         addChild(Lcd::createLcd<Qqqq>(mm2px(Vec(27.6f, 21.2f)), module));
 
         // Scale, Key, External
-        addParam(createParam<AriaKnob820ScaleQqqq>(mm2px(Vec(25.f, 29.f)), module, Qqqq::SCALE_PARAM));
-        addParam(createParam<AriaKnob820ScaleQqqq>(mm2px(Vec(35.f, 29.f)), module, Qqqq::KEY_PARAM));
+        addParam(createParam<QqqqWidgets::ScaleKnob>(mm2px(Vec(25.f, 29.f)), module, Qqqq::SCALE_PARAM));
+        addParam(createParam<QqqqWidgets::ScaleKnob>(mm2px(Vec(35.f, 29.f)), module, Qqqq::KEY_PARAM));
         addInput(createInput<AriaJackIn>(mm2px(Vec(45.f, 29.f)), module, Qqqq::EXT_SCALE_INPUT));
         addOutput(createOutput<AriaJackOut>(mm2px(Vec(55.f, 29.f)), module, Qqqq::EXT_SCALE_OUTPUT));
 
@@ -465,8 +518,8 @@ struct QqqqWidget : ModuleWidget {
         addInput(createInput<AriaJackIn>(mm2px(Vec(84.f, 53.f)), module, Qqqq::SCENE_INPUT));
 
         // Keyboard/Clipboard inputs
-        addParam(createParam<AriaPushButtonKeyboard>(mm2px(Vec(83.f, 64.f)), module, Qqqq::KEYBOARD_INPUT_PARAM));
-        addParam(createParam<AriaPushButtonClipboard>(mm2px(Vec(83.f, 73.f)), module, Qqqq::PASTE_CLIPBOARD_PARAM));
+        addParam(createParam<QqqqWidgets::PushButtonKeyboard>(mm2px(Vec(83.f, 64.f)), module, Qqqq::KEYBOARD_INPUT_PARAM));
+        addParam(createParam<QqqqWidgets::PushButtonClipboard>(mm2px(Vec(83.f, 73.f)), module, Qqqq::PASTE_CLIPBOARD_PARAM));
 
         // The quantizer columns
         drawQuantizerColumn(25.f, 43.f, module, 0);
